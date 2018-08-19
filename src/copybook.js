e5b78df4d91d0c6100b6d1ccabeb7188e5822a10
Joabe Costa <joabetc@hotmail.com>
2018-08-18 23:07:04 -0300

    Chaging to old implementation

--- src/copybook.js
+++ src/copybook.js
@@ -1,11 +1,6 @@
 class CopyBook {
   toArray(copybook) {
-    return copybook.split('\r\n').reduce((acum, curr) => {
-      if (curr.substr(6, 1) !== '*' && !(/^( )+$/g).test(curr)) {
-        acum.push(curr.substr(6, curr.lenght - 6));
-      }
-      return acum;
-    },[]).join('\r\n');
+    return copybook.split('\r\n');
   }
 }
 
