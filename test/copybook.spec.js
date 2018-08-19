65025fab144730eaac8adf3ddbe3240e9661dee9
Joabe Costa <joabetc@hotmail.com>
2018-08-18 23:07:54 -0300

    Using file from original project sample input

--- test/copybook.spec.js
+++ test/copybook.spec.js
@@ -1,13 +1,15 @@
 var copybook = require('../src/copybook');
 var assert = require('assert');
+var fs = require('fs');
+var path = require('path');
+var nomeArquivo = (fs.readdirSync('./src/input_file')[0] || '');
+var arqEnt = ((fs.readFileSync(path.join('./src/input_file', nomeArquivo), 'utf8')) || '');
 
 describe('copybook', function() {
 
-  var mockFile = 'teste\r\nteste\r\nteste';
-
   context('when converting copybook into an array of lines', function() {
-    it('should return 3 as array lenght for a text of 3 line breaks', function() {
-      assert.equal(copybook.toArray(mockFile).length, 3)
+    it('should return 229 as array lenght for a text of 229 line breaks', function() {
+      assert.equal(copybook.toArray(arqEnt).length, 229);
     });
   });
 });
