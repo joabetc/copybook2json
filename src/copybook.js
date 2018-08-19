2ec15a22b1953bc0103dd692ba8da2aece58125f
Joabe Costa <joabetc@hotmail.com>
2018-08-18 23:00:21 -0300

    Rewrite legacy code without lodash

--- src/copybook.js
+++ src/copybook.js
@@ -1,6 +1,11 @@
 class CopyBook {
   toArray(copybook) {
-    return copybook.split('\r\n');
+    return copybook.split('\r\n').reduce((acum, curr) => {
+      if (curr.substr(6, 1) !== '*' && !(/^( )+$/g).test(curr)) {
+        acum.push(curr.substr(6, curr.lenght - 6));
+      }
+      return acum;
+    },[]).join('\r\n');
   }
 }
 
