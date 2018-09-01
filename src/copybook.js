b637d4c15eb75d74c5c64b8b08082aafe81366ed
Joabe Costa <joabetc@hotmail.com>
2018-09-01 10:41:41 -0300

    Segregating code in pieces by its responsibility

--- src/copybook.js
+++ src/copybook.js
@@ -1,18 +1,32 @@
 const COMMENT_COLUMN_NUMBER = 6;
 const COMMENT_CHAR = '*';
+const LINE_BREAK = '\r\n';
+const BLANK_SPACE = ' ';
 
 class CopyBook {
-  _linesToArray(copybook) {
-    return copybook.split('\r\n');
+  _linesToArray(copybook, regex) {
+    return copybook.split(regex);
   }
-  _arrayToLines(array) {
-    return array.join('\r\n');
+  _arrayToLines(array, regex) {
+    return array.join(regex);
   }
   _isBookComment(line) {
     return (line.substr(COMMENT_COLUMN_NUMBER, 1) == COMMENT_CHAR);
   }
   _isBlankLine(line) {
     return (/^( )+$/g).test(line);
+  }
+  _regexReplace(target, regex, newValue) {
+    return target.replace(new RegExp(regex, 'g'), newValue);
+  }
+  _extractDeclarations(copyBookInput) {
+    return this._linesToArray(
+      this._arrayToLines(
+        this._removeComments(
+          this._linesToArray(copyBookInput, LINE_BREAK)
+        ),
+      LINE_BREAK
+    ), '.');
   }
   _removeComments(bookLines) {
     return bookLines.reduce((acum, curr) => {
@@ -25,35 +39,39 @@ class CopyBook {
       return acum;
     },[]);
   }
-  _regexReplace(target, regex, newValue) {
-    return target.replace(new RegExp(regex, 'g'), newValue);
+  _removeCopybookLineBreaks(bookLine) {
+    return this._regexReplace(bookLine, '(\r\n)(\-)( )+(\')', '');
   }
-  _extractDeclarations(copyBookInput) {
-    return this._arrayToLines(
-      this._removeComments(
-        this._linesToArray(copyBookInput)
-      )
-    ).split('.');
+  _removeMultipleLineBreaks(bookLine) {
+    return this._regexReplace(bookLine, '(\r\n)+', '');
+  }
+  _normalizeSpaces(bookLine) {
+    return this._regexReplace(bookLine, '( )+', ' ');
+  }
+  _prepareDefaultValues(bookLine) {
+    let regex = new RegExp('(\')(.+)(\')', 'g');
+    let defaultValue = bookLine.match(regex);
+    if (defaultValue) {
+      bookLine = bookLine.replace(regex, '***');
+    }
+    bookLine = this
+      ._normalizeSpaces(bookLine)
+      .trim();
+    bookLine = this._linesToArray(bookLine, BLANK_SPACE);
+    if (defaultValue) {
+      regex = new RegExp('[\*]{3}', 'g');
+      bookLine = bookLine.map((curr, key) => 
+        curr.replace(regex, defaultValue)
+      );
+    }
+    return bookLine;
   }
   bookToList(copyBookInput) {
     let result = this._extractDeclarations(copyBookInput)
       .reduce((acum, curr, key) => {
-        let result = this._regexReplace(curr, '(\r\n)(\-)( )+(\')', '');
-        result = this._regexReplace(result, '(\r\n)+', '');
-        let regex = new RegExp('(\')(.+)(\')', 'g');
-        let defaultValue = result.match(regex);
-        if (defaultValue) {
-          result = result.replace(regex, '***');
-        }
-        result = this._regexReplace(result, '( )+', ' ');
-        result = result.trim();
-        result = result.split(' ');
-        if (defaultValue) {
-          regex = new RegExp('[\*]{3}', 'g');
-          result = result.map((curr, key) => 
-            curr.replace(regex, defaultValue)
-          );
-        }
+        let result = this._removeCopybookLineBreaks(curr);
+        result = this._removeMultipleLineBreaks(result);
+        result = this._prepareDefaultValues(result);
         if (result[0].length > 0) {
           acum.push(result);
         }
